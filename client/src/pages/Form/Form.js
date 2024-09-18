eb0218b7fe03cdadf62b38c27546cc1b8438c174
Ashish050488 <ashishar050488@gmail.com>
2024-09-18 23:41:55 +0530

    started working on login

---
+++ client/src/pages/Form/Form.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Form = () => {
+  return (
+    <div className='min-h-screen bg-black w-full flex justify-center items-center'>
+      <div className='bg-gray-200 w-8/12 xl:w-4/12 h-[38rem] shadow-lg rounded-lg flex flex-col p-2 m-2 justify-center items-center'>
+      <div className='text-4xl font-bold'>Welcome</div>
+      <div className='text-xl font-light text-gray-400'>Sign up to get started</div>
+      </div>
+    </div>
+  );
+};
+
+export default Form;
